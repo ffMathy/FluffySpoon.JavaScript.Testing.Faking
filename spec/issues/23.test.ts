2957aeacd03bb008e694719d09c4fb93a2a1e5ee
thrixton <johnbaro@gmail.com>
2019-03-07 09:50:11 +1100

    Update test for received invokes method (#26)

--- spec/issues/23.test.ts
+++ spec/issues/23.test.ts
@@ -1,23 +1,23 @@
-import test from 'ava';
+import test from "ava";
 
-import { Substitute, Arg } from '../../src/Index';
+import { Substitute, Arg } from "../../src/Index";
 
 interface CalculatorInterface {
-    add(a: number, b: number): number
-    subtract(a: number, b: number): number
-    divide(a: number, b: number): number
-    isEnabled: boolean
+  add(a: number, b: number): number;
+  subtract(a: number, b: number): number;
+  divide(a: number, b: number): number;
+  isEnabled: boolean;
 }
 
-test('issue 23: mimick received should not call method', t => {
-    const mockedCalculator = Substitute.for<CalculatorInterface>();
+test("issue 23: mimick received should not call method", t => {
+  const mockedCalculator = Substitute.for<CalculatorInterface>();
 
-    let result = 0;
-    mockedCalculator.add(Arg.all()).mimicks((a, b) => {
-        return result = a + b;
-    });
+  mockedCalculator.add(Arg.all()).mimicks((a, b) => {
+    t.deepEqual(a, 1);
+    return a + b;
+  });
 
-    t.throws(() => mockedCalculator.received().add(Arg.any(), Arg.any()));
+  mockedCalculator.add(1, 1); // ok
 
-    t.is(result, 0);
+  mockedCalculator.received(1).add(2, 1); // not ok, calls mimick func
 });
