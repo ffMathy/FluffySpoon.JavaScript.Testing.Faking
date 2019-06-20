1e9f15c5f8dec197d1f614e07e316b05c727050d
Renato Mariscal <renatomariscal@users.noreply.github.com>
2019-06-19 22:37:56 -0700

    Removing dist from source (#52)

--- dist/src/Utilities.d.ts
+++
@@ -1,5 +0,0 @@
-export declare type Call = any[];
-export declare function stringifyArguments(args: any[]): string;
-export declare function areArgumentArraysEqual(a: any[], b: any[]): boolean;
-export declare function stringifyCalls(calls: Call[]): string;
-export declare function areArgumentsEqual(a: any, b: any): boolean;
--- dist/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Substitute, SubstituteOf } from './Substitute';
-export { Arg } from './Arguments';
-export { Substitute, SubstituteOf };
-export default Substitute;
