e43a266b77b9c651ef5d13559441203636aba959
Ross Hendry <rhendry@googlemail.com>
2019-09-26 10:25:38 +0100

    Test case for optional parameters (#61)

---
+++ spec/issues/59.test.ts
@@ -0,0 +1,17 @@
+import test from 'ava';
+
+import { Substitute } from '../../src/index';
+
+interface IEcho {
+  echo(a: string): string
+  maybeEcho(a?: string): string
+}
+
+test('issue 59 - Mock function with optional parameters', (t) => {
+  const echoer = Substitute.for<IEcho>()
+  echoer.maybeEcho('foo').returns('bar')
+  echoer.maybeEcho().returns('baz')
+
+  t.is(echoer.maybeEcho('foo'), 'bar')
+  t.is(echoer.maybeEcho(), 'baz')
+})
