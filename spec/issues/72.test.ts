f4aeb15349311780e6406e398df4f6e517381f99
Eugene Shevchenko <evgenii.shevchenko@fulldive.com>
2019-12-07 14:09:07 +0300

    Add t.pass()

--- spec/issues/72.test.ts
+++ spec/issues/72.test.ts
@@ -10,42 +10,53 @@ interface Calculator {
   isEnabled: boolean;
 }
 
-test("check didNotReceive after not mocking and not calling a method", () => {
+test("check didNotReceive after not mocking and not calling a method", t => {
   const calculator = Substitute.for<Calculator>();
 
   // Do not mock and do not call
   calculator.didNotReceive().add(1, 2);
   calculator.didNotReceive().add(Arg.any(), Arg.any());
   calculator.didNotReceive().add(1, Arg.any());
+
+  t.pass();
 });
 
-test("check didNotReceive after not mocking but calling a method", () => {
+test("check didNotReceive after not mocking but calling a method", t => {
   const calculator = Substitute.for<Calculator>();
 
   // Do not mock, but call
   calculator.add(1, 2);
+
   calculator.didNotReceive().add(1, 2);
   calculator.didNotReceive().add(Arg.any(), Arg.any());
   calculator.didNotReceive().add(1, Arg.any());
+
+  t.pass();
 });
 
-test("check didNotReceive after mocking but not calling a method", () => {
+test("check didNotReceive after mocking but not calling a method", t => {
   const calculator = Substitute.for<Calculator>();
 
   // Mock but do not call
   calculator.add(1, 2).returns(3);
+
   calculator.didNotReceive().add(1, 2);
   calculator.didNotReceive().add(Arg.any(), Arg.any());
   calculator.didNotReceive().add(1, Arg.any());
+
+  t.pass();
 });
 
-test("check didNotReceive after mocking and calling a method", () => {
+test("check didNotReceive after mocking and calling a method", t => {
   const calculator = Substitute.for<Calculator>();
 
   // Mock and call
   calculator.add(1, 2).returns(3);
   calculator.add(1, 2);
+
   calculator.didNotReceive().add(1, 2);
   calculator.didNotReceive().add(Arg.any(), Arg.any());
   calculator.didNotReceive().add(1, Arg.any());
+
+  t.pass();
 });
