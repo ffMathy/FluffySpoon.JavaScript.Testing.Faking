6bbc31d9929cfcb3e98b513ed82c8af99652e569
Enrique Pöhlmann <sm@notanengineer.com>
2020-02-14 00:04:58 +0100

    return substitute string for util.inspect

--- src/states/InitialState.ts
+++ src/states/InitialState.ts
@@ -8,8 +8,8 @@ import { AreProxiesDisabledKey } from "../Substitute";
 export class InitialState implements ContextState {
     private recordedGetPropertyStates: Map<PropertyKey, GetPropertyState>;
     private recordedSetPropertyStates: SetPropertyState[];
-    
-    private _expectedCount: number|undefined|null;
+
+    private _expectedCount: number | undefined | null;
     private _areProxiesDisabled: boolean;
 
     public get expectedCount() {
@@ -47,28 +47,28 @@ export class InitialState implements ContextState {
     }
 
     assertCallCountMatchesExpectations(
-      calls: Call[], // list of arguments
-      callCount: number,
-      type: Type, // method or property
-      property: PropertyKey,
-      args: any[]
+        calls: Call[], // list of arguments
+        callCount: number,
+        type: Type, // method or property
+        property: PropertyKey,
+        args: any[]
     ) {
         const expectedCount = this._expectedCount;
 
         this.clearExpectations();
-        if(this.doesCallCountMatchExpectations(expectedCount, callCount))
+        if (this.doesCallCountMatchExpectations(expectedCount, callCount))
             return;
-    
+
         throw new Error(
-            'Expected ' + (expectedCount === null ? '1 or more' : expectedCount) + 
-            ' call' + (expectedCount === 1 ? '' : 's') + ' to the ' + type + ' ' + property.toString() + 
-            ' with ' + stringifyArguments(args) + ', but received ' + (callCount === 0 ? 'none' : callCount) + 
-            ' of such call' + (callCount === 1 ? '' : 's') + 
+            'Expected ' + (expectedCount === null ? '1 or more' : expectedCount) +
+            ' call' + (expectedCount === 1 ? '' : 's') + ' to the ' + type + ' ' + property.toString() +
+            ' with ' + stringifyArguments(args) + ', but received ' + (callCount === 0 ? 'none' : callCount) +
+            ' of such call' + (callCount === 1 ? '' : 's') +
             '.\nAll calls received to ' + type + ' ' + property.toString() + ':' + stringifyCalls(calls)
         );
     }
 
-    private doesCallCountMatchExpectations(expectedCount: number|undefined|null, actualCount: number) {
+    private doesCallCountMatchExpectations(expectedCount: number | undefined | null, actualCount: number) {
         if (expectedCount === void 0)
             return true;
 
@@ -82,7 +82,7 @@ export class InitialState implements ContextState {
     }
 
     set(context: Context, property: PropertyKey, value: any) {
-        if(property === AreProxiesDisabledKey) {
+        if (property === AreProxiesDisabledKey) {
             this._areProxiesDisabled = value;
             return;
         }
@@ -102,10 +102,13 @@ export class InitialState implements ContextState {
 
     get(context: Context, property: PropertyKey) {
         if (typeof property === 'symbol') {
-            if(property === AreProxiesDisabledKey)
+            if (property === AreProxiesDisabledKey)
                 return this._areProxiesDisabled;
 
             if (property === Symbol.toPrimitive)
+                return () => '{SubstituteJS fake}';
+
+            if (property.toString() === 'Symbol(util.inspect.custom)')
                 return () => '{SubstituteJS fake}';
 
             if (property === Symbol.iterator)
@@ -113,8 +116,6 @@ export class InitialState implements ContextState {
 
             if (property === Symbol.toStringTag)
                 return 'Substitute';
-            if(property.toString() === 'Symbol(util.inspect.custom)')
-                return void 0;
         }
 
         if (property === 'valueOf')
