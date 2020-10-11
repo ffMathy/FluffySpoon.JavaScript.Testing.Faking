329a40a495fd66b5237fa54d5ba63166370d1440
Enrique Pöhlmann <pohlmann@stocard.de>
2020-10-11 18:37:52 +0200

    add working returns example

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
-import { Substitute, SubstituteOf } from './Substitute';
+import { Substitute, SubstituteOf } from './Substitute'
 
-export { Arg } from './Arguments';
-export { Substitute, SubstituteOf };
+export { Arg } from './Arguments'
+export { Substitute, SubstituteOf }
 
-export default Substitute;
+export default Substitute
+
+interface Calc {
+  version: number
+  add(a: number, b: number): number
+}
+
+const c = Substitute.for<Calc>()
+
+c.add(2, 3).returns(5)
+console.log(c.add(2, 3))
+
+c.version.returns(123)
+console.log(c.version)
