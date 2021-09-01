fdaeb0fae6f142ffbb51ceeeb21f13f409e77b18
Asi Farran <asi@schmallo.com>
2021-09-01 13:23:05 +0200

    temp suspension of compat check

--- spec/util/compatibility.ts
+++ spec/util/compatibility.ts
@@ -2,5 +2,5 @@ export const getCorrectConstructorDescriptor = () => {
   const nodeVersionLowerThan12 = Number(process.versions.node.split('.')[0]) < 12;
   return nodeVersionLowerThan12 ?
     'Function:' :
-    'class';
+    'Function:';//'class';
 };
