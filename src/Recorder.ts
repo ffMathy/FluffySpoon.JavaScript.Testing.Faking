9190d17a0769a2e292c549617a722103527f88da
Enrique Pöhlmann <sm@notanengineer.com>
2022-10-07 21:01:11 +0200

    refactor recorder and recordsSet

--- src/Recorder.ts
+++ src/Recorder.ts
@@ -1,46 +1,55 @@
 import { inspect, InspectOptions } from 'util'
-import { SubstituteNodeBase } from './SubstituteNodeBase'
+
+import { FilterFunction } from './Types'
 import { RecordsSet } from './RecordsSet'
 
-export class Recorder {
-  private _records: RecordsSet<SubstituteNodeBase>
-  private _indexedRecords: Map<PropertyKey, RecordsSet<SubstituteNodeBase>>
+export class Recorder<TRecord> {
+  private _identity: PropertyKey
+  private _records: RecordsSet<TRecord>
+  private _indexedRecords: Map<PropertyKey, RecordsSet<TRecord>>
 
-  constructor() {
+  private constructor(identity: PropertyKey) {
+    this._identity = identity
     this._records = new RecordsSet()
     this._indexedRecords = new Map()
   }
 
-  public get records(): RecordsSet<SubstituteNodeBase> {
+  public static withIdentityProperty<TRecord>(identity: keyof TRecord): Recorder<TRecord> {
+    return new this(identity)
+  }
+
+  public get records(): RecordsSet<TRecord> {
     return this._records
   }
 
-  public get indexedRecords(): Map<PropertyKey, RecordsSet<SubstituteNodeBase>> {
+  public get indexedRecords(): Map<PropertyKey, RecordsSet<TRecord>> {
     return this._indexedRecords
   }
 
-  public addIndexedRecord(node: SubstituteNodeBase): void {
-    this.addRecord(node)
-    const existingNodes = this.indexedRecords.get(node.key)
-    if (typeof existingNodes === 'undefined') this.indexedRecords.set(node.key, new RecordsSet([node]))
-    else existingNodes.add(node)
+  public addIndexedRecord(record: TRecord): void {
+    const id = this.getIdentity(record)
+    this.addRecord(record)
+    const existingNodes = this.indexedRecords.get(id)
+    if (typeof existingNodes === 'undefined') this.indexedRecords.set(id, new RecordsSet([record]))
+    else existingNodes.add(record)
   }
 
-  public addRecord(node: SubstituteNodeBase): void {
-    this._records.add(node)
+  public addRecord(record: TRecord): void {
+    this._records.add(record)
   }
 
-  public getSiblingsOf(node: SubstituteNodeBase): RecordsSet<SubstituteNodeBase> {
-    const siblingNodes = this.indexedRecords.get(node.key) ?? new RecordsSet()
-    return siblingNodes.filter(siblingNode => siblingNode !== node)
+  public getSiblingsOf(record: TRecord): RecordsSet<TRecord> {
+    const siblings = this.indexedRecords.get(this.getIdentity(record)) ?? new RecordsSet()
+    return siblings.filter(sibling => sibling !== record)
   }
 
-  public clearRecords(filterFunction: (node: SubstituteNodeBase) => boolean) {
+  public clearRecords(filterFunction: FilterFunction<TRecord>) {
     const recordsToRemove = this.records.filter(filterFunction)
     for (const record of recordsToRemove) {
-      const indexedRecord = this.indexedRecords.get(record.key)
+      const id = this.getIdentity(record)
+      const indexedRecord = this.indexedRecords.get(id)
       indexedRecord.delete(record)
-      if (indexedRecord.size === 0) this.indexedRecords.delete(record.key)
+      if (indexedRecord.size === 0) this.indexedRecords.delete(id)
       this.records.delete(record)
     }
   }
@@ -50,5 +59,9 @@ export class Recorder {
     return entries.map(
       ([key, value]) => `\n  ${key.toString()}: {\n${[...value.map(v => `    ${inspect(v, options)}`)].join(',\n')}\n  }`
     ).join()
+  }
+
+  private getIdentity(record: TRecord): PropertyKey {
+    return record[this._identity as keyof TRecord] as PropertyKey
   }
 }
